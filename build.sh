48afcde6b3024d5472ce143100a1e2bab77beb67
Tentenaizen <ariesbsds.ai@gmail.com>
2026-08-08 13:32:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 set -e
 pip install -r requirements.txt
 
-# Download & install FFmpeg static build
-cd /app
+# Download FFmpeg to the project directory
 curl -O https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
 tar xf ffmpeg-release-amd64-static.tar.xz
 mv ffmpeg-*-amd64-static ffmpeg
